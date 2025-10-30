6536a9b5d2e82a927538f776da7ab6819b6a3d70
0x1306a94 <0x1306a94@gmail.com>
2025-10-30 17:27:21 +0800

    use wcdb prebuilt

--- Frameworks/Storage/Package.swift
+++ Frameworks/Storage/Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Lakr233/MarkdownView", from: "3.4.2"),
-        .package(url: "https://github.com/Tencent/wcdb", from: "2.1.11"),
+//        .package(url: "https://github.com/Tencent/wcdb", from: "2.1.11"),
+        .package(url: "https://github.com/0x1306a94/wcdb-spm-prebuilt", from: "2.1.14"),
         .package(url: "https://github.com/apple/swift-collections", from: "1.2.0"),
         .package(url: "https://github.com/weichsel/ZIPFoundation", from: "0.9.19"),
         .package(path: "../Logger"),
@@ -21,7 +22,8 @@ let package = Package(
     targets: [
         .target(name: "Storage", dependencies: [
             .product(name: "MarkdownParser", package: "MarkdownView"),
-            .product(name: "WCDBSwift", package: "wcdb"),
+//            .product(name: "WCDBSwift", package: "wcdb"),
+            .product(name: "WCDBSwift", package: "wcdb-spm-prebuilt"),
             .product(name: "OrderedCollections", package: "swift-collections"),
             .product(name: "ZIPFoundation", package: "ZIPFoundation"),
             .product(name: "Logger", package: "Logger"),
